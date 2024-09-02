3886acdc142e24a0a9f87e7b6a5af2470ddac9b5
MariaBu1701 <maria-lara.burkart@outlook.de>
2024-09-02 21:39:11 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const card = document.querySelector('.card');
-
-document.addEventListener('mousemove', function(event) {
-    let rotateY = (event.clientX / window.innerWidth * 360) - 180; // Vollständige 360°-Drehung
-    let rotateX = (event.clientY / window.innerHeight * 20) - 10;  // Kleiner Neigungswinkel nach oben/unten
-    card.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
-});
